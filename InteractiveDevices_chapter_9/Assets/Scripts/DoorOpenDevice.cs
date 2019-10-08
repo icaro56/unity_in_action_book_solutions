bdb665f9cfff364c50f795d05877ec0461c8e45f
Icaro Motta <icaromotta56@gmail.com>
2019-10-07 22:07:38 -0300

    Add chapter 9 codes

---
+++ InteractiveDevices_chapter_9/Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 dPos;
+
+    private bool _open;
+
+    public void Operate()
+    {
+        transform.position += (_open ? -dPos : dPos);
+
+        _open = !_open;
+    }
+
+    public void Activate()
+    {
+        if (!_open)
+        {
+            transform.position += dPos;
+
+            _open = true;
+        }
+    }
+
+    public void Deactivate()
+    {
+        if (_open)
+        {
+            transform.position -= dPos;
+
+            _open = false;
+        }
+    }
+}
