f354354e42cdb08529025c757ae0eee318315317
icaro56 <icarogoulart@yahoo.com.br>
2020-02-26 22:10:25 -0300

    finish part 2

--- InteractiveDevices_chapter_9/Assets/Scripts/InventoryManager.cs
+++ InteractiveDevices_chapter_9/Assets/Scripts/InventoryManager.cs
@@ -8,6 +8,22 @@ public class InventoryManager : MonoBehaviour, IGameManager
 
     private Dictionary<string, int> _items;
 
+    public string equippedItem { get; private set; }
+
+    public bool EquipItem(string name)
+    {
+        if (_items.ContainsKey(name) && equippedItem != name)
+        {
+            equippedItem = name;
+            Debug.Log("Equipped " + name);
+            return true;
+        }
+
+        equippedItem = null;
+        Debug.Log("Unequipped");
+        return false;
+    }
+
     public void Startup()
     {
         Debug.Log("Inventory manager starting ...");
@@ -40,5 +56,41 @@ public class InventoryManager : MonoBehaviour, IGameManager
         }
 
         DisplayItems();
+    }
+
+    public List<string> GetItemList()
+    {
+        List<string> list = new List<string>(_items.Keys);
+        return list;
+    }
+
+    public int GetItemCount(string name)
+    {
+        if (_items.ContainsKey(name))
+            return _items[name];
+
+        return 0;
+    }
+
+    public bool ConsumeItem(string name)
+    {
+        if (_items.ContainsKey(name))
+        {
+            _items[name]--;
+
+            if (_items[name] == 0)
+            {
+                _items.Remove(name);
+            }
+        }
+        else
+        {
+            Debug.Log("cannot consume " + name);
+            return false;
+        }
+
+        DisplayItems();
+
+        return true;
     }
 }
