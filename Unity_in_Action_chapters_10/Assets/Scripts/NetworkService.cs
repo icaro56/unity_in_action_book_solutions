4def9bc2d9a050bb01d66a4ccf7eaef44871994e
icaro56 <icarogoulart@yahoo.com.br>
2020-08-12 18:46:43 -0300

    cap 10 até load image

---
+++ Unity_in_Action_chapters_10/Assets/Scripts/NetworkService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class NetworkService 
+{
+    private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml&APPID=0feac06ce2300626230653148e048b4c";
+    private const string jsonApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&APPID=0feac06ce2300626230653148e048b4c";
+    private const string webImage = "http://upload.wikimedia.org/wikipedia/commons/c/c5/Moraine_Lake_17092005.jpg";
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.Send();
+
+            if (request.isNetworkError)
+            {
+                Debug.LogError("network problem: " + request.error);
+            }
+            else if (request.responseCode != (long)System.Net.HttpStatusCode.OK)
+            {
+                Debug.LogError("response error: " + request.responseCode);
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlApi, callback);
+    }
+
+    public IEnumerator GetWeatherJSON(Action<string> callback)
+    {
+        return CallAPI(jsonApi, callback);
+    }
+
+    public IEnumerator DownloadImage(Action<Texture2D> callback)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(webImage);
+        yield return request.Send();
+        callback(DownloadHandlerTexture.GetContent(request));
+    }
+}
---
+++ Unity_in_Action_chapters_10/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+using System;
+using MiniJSON;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    private NetworkService _network;
+
+    public float cloudValue { get; private set; }
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather manager stating...");
+
+        _network = service;
+
+        //StartCoroutine(_network.GetWeatherXML(OnXMLDataLoaded));
+        StartCoroutine(_network.GetWeatherJSON(OnJSONDataLoaded));
+
+        status = ManagerStatus.Initializing;
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+
+        XmlNode root = doc.DocumentElement;
+
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100.0f;
+
+        Debug.Log("Value: " + cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+
+        status = ManagerStatus.Started;
+    }
+
+    public void OnJSONDataLoaded(string data)
+    {
+        Dictionary<string, object> dict;
+        dict = Json.Deserialize(data) as Dictionary<string, object>;
+
+        Dictionary<string, object> clouds = (Dictionary<string, object>)dict["clouds"];
+        cloudValue = (long)clouds["all"] / 100.0f;
+
+        Debug.Log("Value: " + cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+
+        status = ManagerStatus.Started;
+    }
+}
